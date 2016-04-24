5e902f7d87a55841c297188941c5bc0be145e487
Thomas Perez <thomas@scullwm.com>
2016-04-24 16:55:55 +0200

    Init commit

---
+++ src/Translala/App/Command/TranslateCommand.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Translala\App\Command;
+
+class TranslateCommand
+{
+
+}
---
+++ src/Translala/Infra/Application/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Translala\Infra\Application;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+    protected function getDefaultCommands()
+    {
+        $commands = parent::getDefaultCommands();
+        $commands[] = new Translala\App\Command\TranslateCommand();
+
+        return $commands;
+    }
+}
